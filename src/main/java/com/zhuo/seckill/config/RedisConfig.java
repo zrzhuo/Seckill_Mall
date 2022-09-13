e30afa2ddc21971f07a9172c6cc9d9b0bdef37eb
zrzhuo <zrzhuo@sina.com>
2022-09-14 00:39:33 +0800

    v1.0

--- src/main/java/com/zhuo/seckill/config/RedisConfig.java
+++ src/main/java/com/zhuo/seckill/config/RedisConfig.java
@@ -6,19 +6,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        // 创建RedisTemplate
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer( ));
+        // 设置连接工厂
         redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // 设置key的自动序列化方式
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        // 设置value的自动序列化方式
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+        // 返回该RedisTemplate
         return redisTemplate;
     }
 }
